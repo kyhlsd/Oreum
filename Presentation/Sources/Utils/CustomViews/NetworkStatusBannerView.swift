7e43e56a254a51b2d540470bf1a673c45e3b2270
kyhlsd <kmyghn@gmail.com>
2025-10-28 15:35:56 +0900

    Feat: Network 배너 위치 수정

--- Presentation/Sources/Utils/CustomViews/NetworkStatusBannerView.swift
+++ Presentation/Sources/Utils/CustomViews/NetworkStatusBannerView.swift
@@ -11,10 +11,9 @@ import SnapKit
 final class NetworkStatusBannerView: BaseView {
 
     private let label = UILabel.create(color: .white, font: AppFont.description)
-    private var heightConstraint: Constraint?
-    private let bannerHeight = 20.0
+    
     private var hideWorkItem: DispatchWorkItem?
-
+    
     // 네트워크 끊김
     func showDisconnected() {
         // 예약된 숨김 작업 취소
@@ -39,7 +38,8 @@ final class NetworkStatusBannerView: BaseView {
 
         // 2초 후 자동으로 숨김
         let workItem = DispatchWorkItem { [weak self] in
-            self?.hide()
+            guard let self else { return }
+            self.hide()
         }
         hideWorkItem = workItem
         DispatchQueue.main.asyncAfter(deadline: .now() + 2.0, execute: workItem)
@@ -48,21 +48,37 @@ final class NetworkStatusBannerView: BaseView {
     // MARK: - Private Methods
 
     private func show() {
-        heightConstraint?.update(offset: bannerHeight)
+        guard let superview = superview else { return }
 
-        UIView.animate(withDuration: 0.3) {
-            self.superview?.layoutIfNeeded()
+        snp.remakeConstraints { make in
+            make.top.equalTo(superview.snp.top)
+            make.horizontalEdges.equalToSuperview()
+            if #available(iOS 26.0, *) {
+                make.bottom.equalTo(superview.safeAreaLayoutGuide.snp.top).offset(-44)
+            } else {
+                make.bottom.equalTo(superview.safeAreaLayoutGuide.snp.top).offset(-40)
+            }
+        }
+
+        UIView.animate(withDuration: 0.3, delay: 0, options: .curveEaseOut) {
+            superview.layoutIfNeeded()
         }
     }
 
     private func hide() {
-        heightConstraint?.update(offset: 0)
+        guard let superview = superview else { return }
 
-        UIView.animate(withDuration: 0.3) {
-            self.superview?.layoutIfNeeded()
+        snp.remakeConstraints { make in
+            make.top.equalTo(superview.snp.top)
+            make.horizontalEdges.equalToSuperview()
+            make.bottom.equalTo(superview.snp.top)
+        }
+
+        UIView.animate(withDuration: 0.3, delay: 0, options: .curveEaseIn) {
+            superview.layoutIfNeeded()
         }
     }
-    
+
     // MARK: - Setups
     override func setupView() {
         clipsToBounds = true
@@ -73,12 +89,8 @@ final class NetworkStatusBannerView: BaseView {
     }
 
     override func setupLayout() {
-        snp.makeConstraints { make in
-            heightConstraint = make.height.equalTo(0).constraint
-        }
-        
         label.snp.makeConstraints { make in
-            make.center.equalToSuperview()
+            make.centerX.bottom.equalToSuperview()
         }
     }
     
